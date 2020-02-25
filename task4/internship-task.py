e2a4a321a382b3a6c8d7deebd39e961ffab682d4
aniket <aniketkumaragrawal.com>
2020-02-25 14:52:28 +0530

    comments

--- task4/internship-task.py
+++ task4/internship-task.py
@@ -6,6 +6,7 @@ def has_no_e():
         if "e"  not in word:
             list_of_words_wo_e.append(word)
     return list_of_words_wo_e
+#retun will just stop the program execution 
 print(has_no_e())
 percentage = (len(list_of_words_wo_e)/len(list_of_sentences))*100;
 print("the percentage of no e words are: " + str(percentage) + "%")
