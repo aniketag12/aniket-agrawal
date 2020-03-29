dccf28dde6efbf19202f05ee96ffb6b85a19f419
aniket <aniketkumaragrawal.com>
2020-03-29 09:59:29 +0530

    new changes have been made

--- main_task/neeltask2.py
+++ main_task/neeltask2.py
@@ -1,30 +1,35 @@
 #import codecs
+
 import os
+import os.path
 
 #for lines in file_read:
-folder_path = r"C:\Users\ANISH\Downloads\bbc\bbc"
-
-category = "business"
-
-file_path = folder_path + "\\" + category
-
-list_of_files = os.listdir(file_path)
-destination_path= r"C:\Users\ANISH\Downloads\bbc_backup\destination_folder"  # removing the path of file
-
-file_path_split_array = file_path.split("\\")
-
-for one_file in list_of_files:
-    source_filename = one_file
-    destination_filename = file_path_split_array[len(file_path_split_array)-1] + "-" + source_filename
-
-    destination_file_path = destination_path + "\\" +  destination_filename
-    source_file_path = file_path + "\\" +  source_filename
-
-    with open(source_file_path, encoding="utf-8") as file_1 , open(destination_file_path , 'w') as file_2:
-        data = file_1.read()
-        new_data = data.replace("\n", "")
-        file_2.write(new_data)
+folder_path = (r"C:\Users\ANISH\Downloads\bbc\bbc")
+destination_path= (r"C:\Users\ANISH\Downloads\bbc_backup\destination_folder")  # removing the path of file
+# if os.path.isdir(folder_path):
+sub_folders_having_dir = [sub_folder for sub_folder in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, sub_folder))]
+ # sub_folder = r"C:\Users\ANISH\Downloads\bbc\bbc" + 
+for category in sub_folders_having_dir :
+    file_path = folder_path + "\\" + category
+    list_of_files = os.listdir(file_path)
+    file_path_split_array = file_path.split("\\")
+    for one_file in list_of_files:
+        source_filename = one_file
+        destination_filename = file_path_split_array[len(file_path_split_array)-1] + "-" + source_filename
+        destination_file_path = destination_path + "\\" +  destination_filename
+        source_file_path = file_path + "\\" +  source_filename
+        with open(source_file_path, encoding="utf-8") as file_1 , open(destination_file_path , 'w') as file_2:
+            data = file_1.read()
+            new_data = data.replace("\n", "")
+            file_2.write(new_data)
+#else:
+    #print("you entered wrong file_path")
+    
+   # 1 both foder path and destination path should be user input
    
+   # 2 to check whether the folder path and destination path is valid
+   # if valid id.dir should return true i.e it should contains files or foldders or not
+   # destination path should already exist or you should be able to create
    
     
     
